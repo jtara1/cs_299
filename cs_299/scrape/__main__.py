3488e45211f949483f8567e2185b70a2951dba3a
James T <jtara@tuta.io>
2018-05-29 12:07:45 -0700

    [scrape] chg: path for dir for json files of tweets is now relative to the src file [ui] chg: default img is now just a white img

--- cs_299/scrape/__main__.py
+++ cs_299/scrape/__main__.py
@@ -7,6 +7,14 @@ import pathlib
 
 
 def download_tweets(screen_name, output_directory='twitter_data'):
+    """Download recent tweets given a twitter username. Saved
+    in the output_directory which is relative to this file
+
+    :param screen_name: username of twitter user
+    :param output_directory: will contain the json file contain \n
+        data on tweets
+    :return: file path the serialized tweets
+    """
     #initialize tweepy and authorize TWITTER
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
@@ -30,7 +38,13 @@ def download_tweets(screen_name, output_directory='twitter_data'):
         for tweet in alltweets
     }
 
+    # take the name of the directory and save it relative to this file
+    output_directory = join(dirname(__file__), basename(output_directory))
+
     file_path = join(output_directory, '{}.json'.format(screen_name.lower()))
+
+    # combines $ mkdir -p /my/path/
+    # $ touch /my/path/file.json
     pathlib.Path(dirname(file_path)).mkdir(parents=True, exist_ok=True)
 
     with open(file_path, 'w') as outfile:
@@ -40,4 +54,5 @@ def download_tweets(screen_name, output_directory='twitter_data'):
 
 
 if __name__ == '__main__':
+    # test
     download_tweets('realDonaldTrump')
